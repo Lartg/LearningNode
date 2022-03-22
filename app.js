7d6f3e9fc450604618579fcc2e3a6594fd5700e6
Lartg <roc.gerrety@gmail.com>
2022-03-21 17:30:21 -0700

    followed instructions perfectly, none of it works...

--- app.js
+++ app.js
@@ -5,10 +5,16 @@ const express = require('express');
 const app = express();
 
 // Middleware
+const exphbs = require('express-handlebars');
+
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
 
 // Routes
 app.get('/', (req, res) => {
-  res.send('Hello World')
+  const gifUrl = 'https://media1.tenor.com/images/561c988433b8d71d378c9ccb4b719b6c/tenor.gif?itemid=10058245'
+  res.render('hello-gif', { gifUrl })
 });
 
 // Start Server
